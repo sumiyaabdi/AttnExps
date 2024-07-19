get bar masking correct, add bar width and ecc to settings, change cue so its not letter,chance opacity of large stim per trial, change bar contrast per trial

TrialBased/session.py
@@ -7,8 +7,8 @@ Created on Mon Feb 25 14:05:10 2019
 """
 
 import numpy as np
+np.random.seed(2024)
 import os
-import random
 
 import sys
 sys.path.append('../../exptools2')
@@ -36,6 +36,7 @@ class AttnSession(PylinkEyetrackerSession):
                          eyetracker_on=eyetracker_on)
 
         self.n_trials = 150
+        self.conds = np.random.randint(1,15,self.n_trials)
         self.stim_per_trial = self.settings['attn_task']['stim_per_trial']
         self.n_stim = self.n_trials * self.stim_per_trial
         self.trials = []
@@ -51,7 +52,6 @@ class AttnSession(PylinkEyetrackerSession):
         else:
             self.screen = np.array([self.win.size[0], self.win.size[1]])
 
-
         #if we are scanning, here I set the mri_trigger manually to the 't'. together with the change in trial.py, this ensures syncing
         if self.settings['PRF stimulus settings']['Scanner sync']==True:
             self.mri_trigger='t'
@@ -62,28 +62,7 @@ class AttnSession(PylinkEyetrackerSession):
                 os.mkdir(self.screen_dir)
 
     def create_stimuli(self):
-        outer_mask = filters.makeMask(matrixSize=self.screen[0],
-                                shape='raisedCosine', 
-                                radius=np.array([self.screen[1]/self.screen[0], 1.0]),
-                                center=(0.0, 0.0), 
-                                range=[-1, 1], 
-                                fringeWidth=0.02
-                                )
-
-        self.outer_mask_stim = visual.GratingStim(self.win, 
-                                        mask=-outer_mask, 
-                                        tex=None, 
-                                        units='pix',
-                                        size=self.screen,
-                                        pos = np.array((0.0,0.0)), 
-                                        color = [0,0,0])
-        
-        self.inner_mask_stim = visual.Circle(self.win, 
-                                    contrast=1,
-                                    radius=2, 
-                                    fillColor=0, 
-                                    edges='circle')
-        
+                
         self.hemistim = HemiFieldStim(session=self, 
                                         angular_cycles=self.settings['radial'].get('angular_cycles'), 
                                         angular_res=self.settings['radial'].get('angular_res'), 
@@ -91,7 +70,9 @@ class AttnSession(PylinkEyetrackerSession):
                                         border_radius=self.settings['radial'].get('border_radius'), 
                                         pacman_angle=self.settings['radial'].get('pacman_angle'), 
                                         n_mask_pixels=self.settings['radial'].get('n_mask_pixels'), 
-                                        frequency=self.settings['radial'].get('frequency'))
+                                        frequency=self.settings['radial'].get('frequency'),
+                                        eccentricity=self.settings['radial']['annulus_eccentricity'],
+                                        width=self.settings['radial']['annulus_width'])
 
         self.largeAF = AttSizeStim(self,
                                    n_sections=self.settings['large_task']['n_sections'],
@@ -136,10 +117,28 @@ class AttnSession(PylinkEyetrackerSession):
                                  start=[-self.screen[1], -self.screen[1]],
                                  end=[self.screen[1], self.screen[1]]
                                  )
+        
+        # create fixation lines
+        self.cue_line1 = visual.Line(win=self.win,
+                                 units="deg",
+                                 lineColor=1,
+                                 lineWidth=self.settings['fixation stim']['line_width'],
+                                 contrast=self.settings['fixation stim']['contrast'],
+                                 start=[-2,-2],
+                                 end=[2,2]
+                                 )
+
+        self.cue_line2 = visual.Line(win=self.win,
+                                 units="deg",
+                                 lineColor=1,
+                                 lineWidth=self.settings['fixation stim']['line_width'],
+                                 contrast=self.settings['fixation stim']['contrast'],
+                                 start=[-2,2],
+                                 end=[2,-2]
+                                 )
     def create_trials(self):
         """include each trial detail (i.e. trial type, task, cue, stimulus displyed)
         """
-
         for i in range(self.n_trials):
             if i in self.settings['attn_task']['blank_trials_pos']:
                 self.trials.append(BlankTrial(session=self,
@@ -147,10 +146,9 @@ class AttnSession(PylinkEyetrackerSession):
                                                 ))
                 continue
             else:
-                cond=str(random.randint(1,15))
                 self.trials.append(AttnTrial(session=self,
                                             trial_nr=i,
-                                            **self.settings['trial_types'][cond]
+                                            **self.settings['trial_types'][str(self.conds[i])]
                                             ))
                 
                 # self.trials.append(AttnTrial(session=self,
@@ -162,21 +160,20 @@ class AttnSession(PylinkEyetrackerSession):
                 #                             mapper_contrast=0.8
                 #                             ))
     
-    def draw_small_stimulus(self):
+    def draw_small_stimulus(self,opacity=1):
         self.stim_nr = self.current_trial.trial_nr
         self.fix_circle.draw(0, radius=self.settings['small_task'].get('radius'))
-        self.smallAF.draw(self.small_balances[self.stim_nr], self.stim_nr)
+        self.smallAF.draw(self.small_balances[self.stim_nr], self.stim_nr,opacity)
     
     def draw_large_stimulus(self,opacity=1):
         self.stim_nr = self.current_trial.trial_nr
         # self.fix_circle.draw(0, radius=self.settings['small_task'].get('radius'))
-        self.largeAF.draw(self.large_balances[self.stim_nr], self.stim_nr)
+        self.largeAF.draw(self.large_balances[self.stim_nr], self.stim_nr,opacity)
         # self.smallAF.draw(self.small_balances[self.stim_nr], self.stim_nr)
 
     def draw_mapper(self,contrast=1):
-        self.hemistim.draw()
-        self.inner_mask_stim.draw()
-        self.outer_mask_stim.draw()
+        self.hemistim.draw(contrast)
+        # self.inner_mask_stim.draw()
 
         # need to redraw fixations because the masking covers it
         self.line1.draw() # fixation guides

TrialBased/stim.py
@@ -132,7 +132,7 @@ class AttSizeStim():
         self.color_orders = np.array(self.color_orders)
 
 
-    def draw(self, color_balance, stim_nr):
+    def draw(self, color_balance, stim_nr,opacity):
         if self.jitter != None:
             self.element_array_stim.setXYs(self.element_array_np[:,[0,1]]+self.j[stim_nr])
 
@@ -145,6 +145,7 @@ class AttSizeStim():
 
         self.element_array_np[this_ring_bool, 5:8] = ordered_signals
         self.element_array_stim.setColors(ordered_signals, log=False)
+        self.element_array_stim.setOpacities(opacity)
 
         self.element_array_stim.draw()
 
@@ -179,7 +180,9 @@ class HemiFieldStim(object):
                 border_radius, 
                 pacman_angle=20,
                 n_mask_pixels=1000,
-                frequency=8.0):
+                frequency=8.0,
+                eccentricity=3.0,
+                width=1):
         
         self.session = session
         self.angular_cycles = angular_cycles
@@ -189,6 +192,8 @@ class HemiFieldStim(object):
         self.pacman_angle = pacman_angle
         self.n_mask_pixels = n_mask_pixels
         self.frequency = frequency
+        self.eccentricity = eccentricity
+        self.width = width
 
         # cosine mask, blurs the edges of the stimulus
         mask = np.ones((n_mask_pixels))
@@ -196,8 +201,8 @@ class HemiFieldStim(object):
         mask[:int(border_radius*n_mask_pixels)] = 0 #(np.cos(np.linspace(0,np.pi,int(border_radius*n_mask_pixels))[::-1])+1)/2
 
         self.stimulus1 = RadialStim(win=self.session.win, 
-                                    mask=mask, 
-                                    size=(50,50),#(1000.0, 1000.0), 
+                                    mask='raisedCos', 
+                                    size=((self.eccentricity+self.width)*2,(self.eccentricity+self.width)*2),#(1000.0, 1000.0), 
                                     radialCycles=self.radial_cycles, 
                                     angularCycles=self.angular_cycles, 
                                     texRes=128, 
@@ -205,11 +210,10 @@ class HemiFieldStim(object):
                                     visibleWedge=(0,360), 
                                     pos=(0.0, 0.0),
                                     ori=180,
-                                    color=1,
-                                    opacity=0.3)
+                                    color=1)
         self.stimulus2 = RadialStim(win=self.session.win, 
-                                    mask=mask, 
-                                    size=(50,50),#(1000.0, 1000.0), 
+                                    mask='raisedCos', 
+                                    size=((self.eccentricity+self.width)*2,(self.eccentricity+self.width)*2), 
                                     radialCycles=self.radial_cycles, 
                                     angularCycles=self.angular_cycles, 
                                     texRes=128, 
@@ -217,40 +221,26 @@ class HemiFieldStim(object):
                                     visibleWedge=(0,360), 
                                     pos=(0.0, 0.0),
                                     ori=180,
-                                    color=-1,
-                                    opacity=0.3)
-#       #JH's stimulus1, stimulus 2
-        # self.stimulus_1 = RadialStim(win=self.session.win, 
-        #                             mask=mask, 
-        #                             size=(1000.0, 1000.0), 
-        #                             radialCycles=self.radial_cycles, 
-        #                             angularCycles=self.angular_cycles, 
-        #                             texRes=128, 
-        #                             angularRes=100, 
-        #                             visibleWedge=(180+pacman_angle, 360-pacman_angle), 
-        #                             pos=(0.0, 0.0),
-        #                             ori=180,
-        #                             color=1)
-        # self.stimulus_2 = RadialStim(win=self.session.win, 
-        #                             mask=mask, 
-        #                             size=(1000.0, 1000.0), 
-        #                             radialCycles=self.radial_cycles,
-        #                             angularCycles=self.angular_cycles, 
-        #                             texRes=128, 
-        #                             angularRes=100, 
-        #                             visibleWedge=(180+pacman_angle, 360-pacman_angle), 
-        #                             pos=(0.0, 0.0),
-        #                             ori=180,
-        #                             color=-1)
+                                    color=-1)
 
-    def draw(self):
+        self.inner_mask_stim = visual.Circle(self.session.win, 
+                            radius=self.eccentricity, 
+                            fillColor=0, 
+                            edges='circle')
+
+
+    def draw(self,contrast):
 
         stim_phase=np.mod(self.session.timer.getTime(), 1.0/self.frequency) * self.frequency
         
-        if stim_phase < 0.5:         
+        if stim_phase < 0.5:      
+            self.stimulus1.setContrast(contrast)   
             self.stimulus1.draw()
+            self.inner_mask_stim.draw()
         else:         
+            self.stimulus2.setContrast(contrast)   
             self.stimulus2.draw()
+            self.inner_mask_stim.draw()
 
 class PRFStim(object):  
     def __init__(self, session, 